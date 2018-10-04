8b423a077f902746b8e3cd7fc0b9043ae4d9cb8e
Yue Huang <kunkka.h@gmail.com>
2018-10-03 23:55:58 -0700

    parse() function half done

---
+++ getword.c
@@ -0,0 +1,161 @@
+/*Yue Huang, Dr Carroll CS570 due: 09/17/2018
+ getword.c is the c file for getword() function
+ getword() function get every word from the commmand input and put them into storage array one by one with a
+ at the end of each word. getword() return length of a string to the caller function.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef enum {
+    false, true
+} bool;
+int getword(char *w)
+{
+    int iochar;
+    bool backslash = false;
+    bool negative = false;  //flag, used to determine whether a string starts with $
+    int wordCount = 0;
+    while ((iochar = getchar()) != EOF) {
+        //case 1: maximum string length
+        if (wordCount == 254) {
+            ungetc(iochar, stdin);  //put back the 255th element
+            * (w + wordCount) = '\0';
+            return wordCount;
+        }
+        //case 2: preceding backslash
+        else if (backslash == true) {
+            backslash = false;
+            if (iochar == '\n') {  //if a backslash preceding a newline, treating it as a space
+               return printOutput(w, wordCount, negative);
+            }
+            * (w + wordCount) = iochar;
+            wordCount++;
+        }
+        //case 3: all other cases
+        else {
+            //case 3.1: starting tilde
+            if (iochar == '~' && wordCount == 0 && negative == false) {
+                char * ptr = getenv("HOME");
+                while(wordCount < strlen(ptr)) {
+                    *(w + wordCount) = * (ptr + wordCount);
+                    wordCount++;
+                }
+                continue;
+            }
+            //case 3.2
+            if (iochar == ' ' || iochar == '\t') {
+                if (wordCount != 0 || negative == true) {  //if it's not leading zero or trailling zeros, print out
+                    return printOutput(w, wordCount, negative);
+                }
+                //if it's leading zeros or trailing zeros, reset flag and continue
+
+                continue;
+            }
+            if (iochar == '$' && wordCount == 0 && negative == false) {
+                negative = true;
+                continue;
+            }
+            //case 3.3
+            if (iochar == ';' || iochar == '\n' ) {
+                if (wordCount != 0 || negative == true) {  //if string is not null or starting with '$'
+                    ungetc(';',stdin);  //if a semicolon or a newline or a '|' follow directly by a string, print out the string
+                    return printOutput(w, wordCount, negative);  //and put semicolon back because we also need to print out an empty string
+                }
+                *w = '\0';  //getting back and print out empty string
+                return 0;
+            }
+            //case 3.4
+            if (iochar == '|') {
+                if (wordCount != 0 || negative == true) {
+                    ungetc('|',stdin);  //get back '|' in order to print out '|'
+                    return printOutput(w, wordCount, negative);  //print out preceding string
+                }
+                * w = '|';
+                * (w + 1) = '\0';
+                return 1;
+            }
+            //case 3.5
+            if (iochar == '>') {
+                if (wordCount != 0 || negative == true) {
+                    ungetc('>',stdin);  //get back '|' in order to print out '|'
+                    return printOutput(w, wordCount, negative);  //print out preceding string
+                }
+                * w = '>';
+                * (w + 1) = '\0';
+                return 1;
+            }
+            //case 3.6
+            if (iochar == '<') {
+                iochar = getchar();  //get the next character first
+                if (iochar == '<') {  //if it's '<<'
+                    if (wordCount != 0 || negative == true) {  //print out preceding string
+                        ungetc('<',stdin);  //put back two '<' in order to print out n=2, s=[<<]
+                        ungetc('<',stdin);
+                        return printOutput(w, wordCount, negative);
+                    }
+                    * w = '<';
+                    * (w + 1) = '<';
+                    * (w + 2) = '\0';
+                    return 2;
+                }
+                else {  //if it's only one '<'
+                    ungetc(iochar, stdin);  //get back next character to stdin stream whatever it's
+                    if (wordCount != 0 || negative == true) {  //print out preceding string
+                        ungetc('<',stdin);  //put back one '<' in order to print out n=1, s=[<]
+                        return printOutput(w, wordCount, negative);
+                    }
+                    * w = '<';
+                    * (w + 1) = '\0';
+                    return 1;
+                }
+            }  //end '<' if
+            //case 3.7: if incoming char is '\'
+            if (iochar == 92) {
+                backslash = true;
+                continue;
+            }
+            //Case 3.8(adding for program2): if incoming char is '&'
+            //If its not at the end of the input, treat it as word separator, otherwise set a global flag
+            //Notice here, \& and & should not be treated the same way. & implies that parent process do not wait for
+            //child process while \& still be treated as a word separator
+            if (iochar == '&') {
+                if (wordCount != 0) {  //print out preceding words first
+                    return printOutput(w, wordCount, negative);
+                } else {
+                    iochar = getchar();
+                    if (iochar == '\n') {  //if & followed by a newline, return -254 with s="&"
+                        ungetc('\n',stdin);
+                        * w = '&';
+                        * (w + 1) = '\0';
+                        return -254;
+                    }
+                    else {
+                        ungetc(iochar, stdin);
+                        * w = '&';
+                        * (w + 1) = '\0';
+                        return 1;
+                    }
+                }
+            }
+            //staring normal parse
+            * (w + wordCount) = iochar;
+            wordCount++;
+        }  //end else
+    }  //end of while loop
+    if (wordCount != 0 || negative == true) {  //if EOF follow directly by a string or by a '$', print out the string
+        return printOutput(w, wordCount, negative);
+    }
+    * w = '\0';
+    return -255;
+}
+int printOutput(char *w, int wordCount, bool negative) {
+    * (w + wordCount) = '\0';  //put a null terminator at the end of each string in order for caller function to print it out
+    if (negative == true) {  //if a string starts with $, then return negative word count;
+        if (wordCount == 0) {
+            return 0;
+        }
+        return -wordCount;
+    } else {
+        return wordCount;
+    }
+}
---
+++ main.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#define MAXWORDS 100
+#include "getword.h"
+#include <math.h>
+typedef enum {
+    false, true
+} bool;
+char *newargv[MAXWORDS];  //argument array
+char s[MAXWORDS*STORAGE];  //storage array
+char * input_file;
+char * output_file;
+int flag_in = 0;  //0 indicating there's no input flag
+int flag_out = 0;   //0 indicating there's no output flag
+int flag_pipeline = 0;   //0 indicating there's no pipeline flag
+int flag_ampersand = 0;   //0 indicating there's no ampersand flag
+int pipeIndex = 0;
+int main(int argc, char * argv[])
+{
+    int num_arg;  //number of arguments returned by parse()
+    int input_fd; //inpu file descriptor
+    int output_fd;  //output file descriptor
+    pid_t childpid;
+    while(true) {
+        printf(":570: ");  //giving the prompt
+        num_arg = parse();
+        if (num_arg == 255) {  //returning 255 if EOF
+            break;
+        }
+        if (num_arg == -2) {
+            fprintf(stderr, "Redirection error.\n");
+            fflush(stderr);
+            continue;
+        }
+    }
+
+}
+int parse() {
+    int argc = 0;  //number of arguments
+    int length = 0;  //used to record how many characters in the storage array, s[]
+    int reValue;  //return value from getword()
+
+    while(true) {
+        reValue = getword(s + length);  //we're not overwrite s array, so we need length here
+        if (reValue == -255) {  //returning 255 if EOF
+            newargv[argc] = NULL;
+            return abs(reValue);
+        }
+        else if (reValue == -254) {  //getting return value -254 if there's '&' at the end
+            flag_ampersand = 1;
+            continue;
+        }
+        else if(reValue == 0) {  //finish collecting words when see '$' '\n' ';'
+            newargv[argc] = NULL;
+            return argc;
+        }
+        else if(reValue == 1) {  //handle metacharacter '|' '>' '<'
+            if (s[length] == '<') {
+                if (flag_in >= 1) {  //if there are more than one '<'
+                    return -2;  //-2 indicating redirection error
+                }
+                else {
+                    flag_in = 1;
+                    length = length + 2;
+                    reValue = getword(s + length);  //immediately checking the following word
+                    //invalid file name ';' '\n' '$' '|' '<' '>' EOF, '<<' be treated as a word so it's valid in this case
+                    if (reValue == 0 ||
+                        s[length] == '|' ||
+                        s[length] == '<' ||
+                        s[length] == '>' ||
+                        reValue == -255) {
+                        return -2;  //-2 indicating redirection error
+                    }
+                    input_file = (s + length);  //pointing input ptr to the string on s string
+                    length = length + abs(reValue) + 1;
+                    argc++;
+                    continue;
+                }
+            }
+            if (s[length] == '>') {
+                if (flag_out >= 1) {  //redirectiion error occurrs when there is more than one '>'
+                    return -2;  //-2 indicating redirection error
+                }
+                else {
+                    flag_out = 1;
+                    length = length + 2;  // ">\0" there are two character so we need to add 2
+                    reValue = getword(s + length);
+                    if (reValue == 0 ||
+                        reValue == -255 ||
+                        s[length] == '|' ||
+                        s[length] == '<' ||
+                        s[length] == '>' ) {
+                        return -2;  //-2 indicating redirection error
+                    }
+                    output_file = (s + length);
+                    length = length + abs(reValue) + 1;
+                    argc++;
+                    continue;
+                }
+            }
+            else {  //otherwise treat this one length character as an argument
+                newargv[argc++] = (s + length);
+                length = length + abs(reValue) + 1;
+                continue;
+            }
+        }
+        else {  // encountering normal words
+            newargv[argc++] = (s + length);
+            length = length + abs(reValue) + 1;  //pay attention here, the return value might be negative because of '$'
+        }
+    }
+
+}
